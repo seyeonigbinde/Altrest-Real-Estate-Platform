4656b93ef1a8ddcd6d2a9d4a939977cdd8ff0d1a
Your Name <you@example.com>
2021-06-06 01:46:54 +0100

    additional styling

--- altrest-app/src/Components/Blog.js
+++ altrest-app/src/Components/Blog.js
@@ -30,6 +30,16 @@ const Blog = ()=> {
                 <img src={altrest} alt="" className="blog_image"/>
             </div>
             </Link>
+            <Link className="blog_container">
+            <div className="blog_content">
+                <h1 className="blog_title">The Story Title</h1>
+                <p className="blog_preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse odio nibh, molestie ut sem non, fermentum imperdiet nisl. Duis ut metus ipsum. Etiam ac placerat tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse auctor sollicitudin metus venenatis semper. Quisque elementum tortor ut metus sodales, vitae pellentesque enim accumsan. Praesent efficitur lorem et elementum varius.  </p>
+                <p className="blog_footer">Date</p>
+            </div>
+            <div>
+                <img src={altrest} alt="" className="blog_image"/>
+            </div>
+            </Link>
         </div>
     )
 }
--- altrest-app/src/Components/Footer.js
+++ altrest-app/src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = ()=> {
     return(
         <footer>
             <p>Copyright &copy; 2021 ::: Designed by Stevens Design Tech.</p>
-            <div>
+            <div className="footer_social">
             <i class="fab fa-facebook-f fa-2x"></i>
             <i class="fab fa-instagram fa-2x"></i>
             <i class="fab fa-twitter fa-2x"></i>
